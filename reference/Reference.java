69fd46ed87dded893f1e203625a41d30baebbf05
expodingpenguin <expodingpenguin@gmail.com>
2014-06-26 13:09:52 -0400

    First publish

---
+++ reference/Reference.java
@@ -0,0 +1,9 @@
+package com.expoding.modularcrafting.reference;
+
+public class Reference {
+
+    public static final String MOD_ID = "ModularCrafting";
+    public static final String MOD_NAME = "Modular Crafting";
+    public static final String VERSION = "1.7.2-1.0";
+
+}
